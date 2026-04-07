e9b9760c2919ce71c9f127f37e2cf97e281cc584
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2026-04-07 18:11:29 +0200

    Make `mix phenom.new` clickable

--- test/phenom_web/controllers/page_controller_test.exs
+++ test/phenom_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhenomWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "mix phenom.new"
   end
 end
